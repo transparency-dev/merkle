c937f3c50f07ba6bf3cd1308b5d4f5b4684e1313
hickford <hickford@google.com>
2022-07-21 12:22:54 +0100

    Fuzz ConsistencyProof function against reference implementation

--- testonly/tree_fuzz_test.go
+++ testonly/tree_fuzz_test.go
@@ -122,3 +122,32 @@ func FuzzInclusionProofAgainstReferenceImplementation(f *testing.F) {
 		}
 	})
 }
+
+func FuzzConsistencyProofAgainstReferenceImplementation(f *testing.F) {
+	for size := 0; size <= 8; size++ {
+		for end := 0; end <= size; end++ {
+			for begin := 0; begin <= end; begin++ {
+				f.Add(uint64(size), uint64(begin), uint64(end))
+			}
+		}
+	}
+	f.Fuzz(func(t *testing.T, size, begin, end uint64) {
+		if size >= math.MaxUint16 {
+			return
+		}
+		t.Logf("size=%d, begin=%d, end=%d", size, begin, end)
+		if begin > end || end > size {
+			return
+		}
+		entries := genEntries(size)
+		tree := newTree(entries)
+		got, err := tree.ConsistencyProof(begin, end)
+		if err != nil {
+			t.Errorf("ConsistencyProof: %v", err)
+		}
+		want := refConsistencyProof(entries[:end], end, begin, tree.hasher, true)
+		if diff := cmp.Diff(got, want, cmpopts.EquateEmpty()); diff != "" {
+			t.Errorf("ConsistencyProof: diff (-got +want)\n%s", diff)
+		}
+	})
+}
