3485627243db280c252ebbbffe8f6ce7eae3062b
hickford <hickford@google.com>
2022-07-22 11:25:59 +0100

    Add GitHub actions for PR fuzzing and continuous builds
    https://google.github.io/clusterfuzzlite/running-clusterfuzzlite/github-actions/

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,2 +1,6 @@
+# https://google.github.io/oss-fuzz/getting-started/new-project-guide/go-lang/#buildsh
+# undocumented dependency
+go install github.com/AdamKorcz/go-118-fuzz-build@latest
 go get github.com/AdamKorcz/go-118-fuzz-build/utils
+# necessary to list each fuzz test explicitly
 compile_native_go_fuzzer github.com/transparency-dev/merkle/compact FuzzRangeNodes FuzzRangeNodes
